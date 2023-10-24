bb857e0e8173474803259124a6a40544d97e3b36
manchedtudk <s215112@student.dtu.dk>
2023-10-24 11:48:27 +0200

    Incorporate data (Task 2.1)

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Standard codon table
+#'
+#' A list of codons with their corresponding one letter codes.
+#'
+#' @format
+#' A dictionary containing 64 key-value pairs:
+#' \describe{
+#'   \item{key}{3 letter codon}
+#'   \item{value}{one letter code}
+#' }
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
