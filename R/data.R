12221f3a1d737249b43fe8211632178b7df6ff89
jennifer-dtu-2023 <s230705@student.dtu.dk>
2023-10-25 23:31:18 +0200

    "Add Roxygen documentation for codon_data and regenerated Roxygen documentation for plot_character_frequency, after correcting typos."

--- R/data.R
+++ R/data.R
@@ -11,3 +11,17 @@
 #'
 #' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
 "codon_table"
+
+#' Codon to Amino Acid Mapping
+#'
+#' A named vector containing the mapping from codons to their corresponding one-letter amino acid codes.
+#'
+#' @format A named vector with 64 elements:
+#' \describe{
+#'   \item{UUU}{F}
+#'   \item{UCU}{S}
+#'   \item{UAU}{Y}
+#'   ...
+#' }
+#' @keywords datasets
+"codon_data"
